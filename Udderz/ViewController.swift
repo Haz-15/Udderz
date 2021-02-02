446bed6daf7d96cc927bc5ca5810bf58d00072f4
Haz-15 <Ab2388@njit.edu>
2021-02-02 12:51:19 -0500

    Added sound for the moo press

--- Udderz/ViewController.swift
+++ Udderz/ViewController.swift
@@ -5,15 +5,41 @@
 //  Created by Asim Burrell on 1/28/21.
 //
 
+import AVFoundation
 import UIKit
+
+
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var player: AVAudioPlayer?
+
+    @IBAction func playSound(_ sender: Any) {
+        play()
+    }
+        
+        func play() {
+            guard let url = Bundle.main.url(forResource: "invalid_keypress", withExtension: "mp3") else { return }
+
+            do {
+                try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+                try AVAudioSession.sharedInstance().setActive(true)
+
+                /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+                player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+                /* iOS 10 and earlier require the following line:
+                player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+                guard let player = player else { return }
+
+                player.play()
+
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
     }
 
 
-}
 
